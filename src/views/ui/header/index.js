35e07ee6865b0aa1157aa148f6a6b72306d12867
Arun Kumar <arunpasupathi24@gmail.com>
2023-04-09 18:15:46 +1000

    chore: deleted unused deprecated code

--- src/views/ui/header/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './style.scss'
-
-const Header = ({title, className}) => {
-  return <h3 className={`ss-header ${className}`}>{title}</h3>
-}
-
-export default Header
