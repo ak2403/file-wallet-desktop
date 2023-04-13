bbdce26816186c86e0e68476e73dfe9d3b3a5da2
Arun Kumar <arunpasupathi24@gmail.com>
2023-04-13 14:03:37 +1000

    fix: workaround fix hot reload issue

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,12 @@ import { RootComponent } from './root-component';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
+//@ts-ignore
+if (module.hot) {
+  //@ts-ignore
+  module.hot.accept();
+}
+
 root.render(
   //@ts-ignore
   <Provider store={Store}>
