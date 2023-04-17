445d78a210878b394035b2e020e0630416a7e55d
Arun Kumar <arunpasupathi24@gmail.com>
2023-04-17 20:09:09 +1000

    chore: removed unused function and console

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -1,45 +1,41 @@
-import { handleError } from "./error"
-const { get, set, remove } = window.store
+import { handleError } from './error';
+const { get, set, remove } = window.store;
 
 export const setItem = async (key, value) => {
   try {
-    console.log('setItem : ', value)
-    await set(key, value)
-    
-    return true
+    await set(key, value);
+
+    return true;
+  } catch (err) {
+    handleError(err);
+    return false;
   }
-  catch(err) {
-    handleError(err)
-    return false
-  }
-}
+};
 
 export const getItem = async (key) => {
   try {
-    const getVal = await get(key)
-    
-    return getVal
+    const getVal = await get(key);
+
+    return getVal;
+  } catch (err) {
+    handleError(err);
+    return false;
   }
-  catch(err) {
-    handleError(err)
-    return false
-  }  
-}
+};
 
 export const removeItem = async (key) => {
   try {
     const getIfItemPresent = get(key);
 
     if (!getIfItemPresent) {
-      return true
+      return true;
     }
-    
-    await remove(key)
-    
-    return true
+
+    await remove(key);
+
+    return true;
+  } catch (err) {
+    handleError(err);
+    return false;
   }
-  catch(err) {
-    handleError(err)
-    return false
-  }  
-}
+};
