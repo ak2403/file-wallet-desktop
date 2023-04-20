77e826800aef87a8bbcb183b8f23d81ca1312a37
Arun Kumar <arunpasupathi24@gmail.com>
2023-04-20 15:59:40 +1000

    chore: add unit test for folder

--- src/views/section/folder-view/file.tsx
+++ src/views/section/folder-view/file.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { faFolder } from '@fortawesome/free-solid-svg-icons';
+import { faFile, faFilePdf, faCode, faFileImage, faFileZipper } from '@fortawesome/free-solid-svg-icons';
+import { faFileWord } from '@fortawesome/free-regular-svg-icons';
 
 import { useSelectedPath } from '../../../hooks-action/connection';
 import { useConnectionId } from '../../../hooks-action/common';
@@ -9,19 +10,51 @@ import { openDialogWindow } from '../../../utils/open-dialog';
 import { FolderLayout, FolderText, ViewIcon } from './folder.styles';
 
 type FileType = {
+  id?: string;
   name: string;
-  id: string;
+  type: string;
 };
 
 export const File: React.FC<FileType> = (props) => {
-  const { name } = props;
+  const { name, type } = props;
   const connectionId = useConnectionId();
   const selectedPath = useSelectedPath();
+
+  const icon = (() => {
+    switch (type) {
+      case '.pdf':
+        return faFilePdf;
+
+      case '.html':
+      case '.css':
+      case '.py':
+      case '.js':
+        return faCode;
+
+      case '.png':
+      case '.jpg':
+        return faFileImage;
+
+      case '.docx':
+      case '.doc':
+        return faFileWord;
+
+      case '.zip':
+      case '.zipx':
+      case '.gz':
+      case '.tar':
+      case '.z':
+        return faFileZipper;
+
+      default:
+        return faFile;
+    }
+  })();
 
   const onClick = async () => {
     const { canceled, filePath: targetFilePath } = await openDialogWindow();
 
-    if (canceled) {
+    if (canceled || !targetFilePath) {
       return;
     }
 
@@ -39,8 +72,8 @@ export const File: React.FC<FileType> = (props) => {
   };
 
   return (
-    <FolderLayout onClick={onClick}>
-      <ViewIcon icon={faFolder} />
+    <FolderLayout data-testid="file" onClick={onClick}>
+      <ViewIcon icon={icon} />
       <FolderText>{name}</FolderText>
     </FolderLayout>
   );
---
+++ src/views/section/folder-view/folder-view.spec.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { FolderView } from './folder-view';
+
+import { useSelectedPath } from '../../../hooks-action/connection';
+import { useConnectionId } from '../../../hooks-action/common';
+import { useFolders } from '../../../hooks-action/folder-structure';
+
+jest.mock('../../../hooks-action/connection', () => ({
+  useSelectedPath: jest.fn(),
+  useUpdateFolderStructure: jest.fn(),
+}));
+jest.mock('../../../hooks-action/common', () => ({
+  useConnectionId: jest.fn(),
+}));
+jest.mock('../../../hooks-action/folder-structure', () => ({
+  useFolders: jest.fn(),
+}));
+jest.mock('../../../utils/open-dialog', () => ({
+  openDialogWindow: jest.fn(),
+}));
+
+(useSelectedPath as jest.Mock).mockReturnValue([
+  {
+    name: 'default path',
+  },
+]);
+(useConnectionId as jest.Mock).mockReturnValue('12345');
+(useFolders as jest.Mock).mockReturnValue([
+  {
+    name: 'mock folder',
+    ext: 'folder',
+  },
+]);
+
+describe('<FolderView />', () => {
+  it('renders folder properly', () => {
+    render(<FolderView />);
+
+    expect(screen.getByTestId('folder')).toBeInTheDocument();
+  });
+
+  it('renders file properly', () => {
+    (useFolders as jest.Mock).mockReturnValue([
+      {
+        name: 'mock file',
+        ext: 'file',
+      },
+    ]);
+
+    render(<FolderView />);
+
+    expect(screen.getByTestId('file')).toBeInTheDocument();
+  });
+
+  it('should not render when no folders present', () => {
+    (useFolders as jest.Mock).mockReturnValue([]);
+
+    render(<FolderView />);
+
+    expect(screen.queryByTestId('folder-view')).not.toBeInTheDocument();
+  });
+});
